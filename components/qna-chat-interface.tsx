f7589b52de29503f38203bb687afbb113ee778f2
Akshat Singh Kushwaha <akshatsingh14372@outlook.com>
2026-03-10 19:50:18 +0530

    fix: Update regex to support additional language formats in QnAChatInterface

--- components/qna-chat-interface.tsx
+++ components/qna-chat-interface.tsx
@@ -407,7 +407,7 @@ export function QnAChatInterface() {
                                                 )
                                             },
                                             code: ({ className, children, ...props }) => {
-                                                const match = /language-(\w+)/.exec(className || "")
+                                                const match = /language-([\w-]+)/.exec(className || "")
                                                 const isChart = match && match[1] === "chart"
                                                 const isDiscordWidget = match && match[1] === "discord-widget"
 
